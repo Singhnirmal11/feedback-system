b3b6bd9eb95f3740a807f9c50c69eaf619561172
Singhnirmal11 <nirmal.nittinsaini@gmail.com>
2026-02-17 18:39:27 +0530

    Added db.js and user registration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 
 const express=require("express"); //tells express framework is needed
 const cors= require("cors");
+const db=require("./db");
 
 const app=express();
 
@@ -11,6 +12,33 @@ app.get("/",(req,res)=>{
   res.send("Server is running");
 });
 
+const bcrypt=require("bcrypt");
+app.post("/register",async(req,res) =>{
+  const {name,email,password}=req.body;
+  
+  try{
+    const hashedPassword= await bcrypt.hash(password,10);
+
+    const query=`
+      INSERT INTO users(name,email,password)
+      VALUES (?,?,?)
+      `;
+
+    db.query(query,[name,email,hashedPassword],(err,result) =>{
+      if(err){
+        console.error(err);
+        return res.status(500).json({message:"Database error"});
+      }
+
+      res.status(201).json({message:"User Registered Successfully"});
+    });
+  }
+  catch(error){
+    res.status(500).json({message:"Server error"});
+  }
+});
+
 app.listen(5001,() =>{
   console.log("Server running on port 5001");
 }); 
+
