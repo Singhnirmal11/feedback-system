2e5f088db322e0a182f13631f7e1a4269b798639
Singhnirmal11 <nirmal.nittinsaini@gmail.com>
2026-02-18 15:31:13 +0530

    JSON Web Token (JWT)

--- server/index.js
+++ server/index.js
@@ -1,44 +1,79 @@
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+const bcrypt = require("bcrypt");
 
-const express=require("express"); //tells express framework is needed
-const cors= require("cors");
-const db=require("./db");
-
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-const bcrypt=require("bcrypt");
-app.post("/register",async(req,res) =>{
-  const {name,email,password}=req.body;
-  
-  try{
-    const hashedPassword= await bcrypt.hash(password,10);
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
 
-    const query=`
-      INSERT INTO users(name,email,password)
-      VALUES (?,?,?)
-      `;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    db.query(query,[name,email,hashedPassword],(err,result) =>{
-      if(err){
-        console.error(err);
-        return res.status(500).json({message:"Database error"});
+    const query = `
+      INSERT INTO users (name, email, password)
+      VALUES (?, ?, ?)
+    `;
+
+    db.query(query, [name, email, hashedPassword], (err, result) => {
+      if (err) {
+        return res.status(500).json({ message: "Database error" });
       }
 
-      res.status(201).json({message:"User Registered Successfully"});
+      res.status(201).json({ message: "User registered successfully" });
     });
-  }
-  catch(error){
-    res.status(500).json({message:"Server error"});
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
   }
 });
 
-app.listen(5001,() =>{
-  console.log("Server running on port 5001");
-}); 
 
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const query = "SELECT * FROM users WHERE email = ?";
+
+  db.query(query, [email], async (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: "Database error" });
+    }
+
+    if (results.length === 0) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    const user = results[0];
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      "secretKey",
+      { expiresIn: "1h" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token: token
+    });
+  });
+});
+
+
+app.listen(5001, () => {
+  console.log("Server running on port 5001");
+});
