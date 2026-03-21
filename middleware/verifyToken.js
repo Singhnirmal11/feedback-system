7bdefc25cca03b47a999cc2d3ca1d0cc71c68a80
Singhnirmal11 <nirmal.nittinsaini@gmail.com>
2026-03-21 23:45:02 +0530

    Structured files in professional way

---
+++ middleware/verifyToken.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const SECRET_KEY = process.env.JWT_SECRET;
+
+function verifyToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+
+  if (!authHeader) {
+    return res.status(403).json({ message: "Token required" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    req.user = decoded;
+    next();
+  });
+}
+
+module.exports = verifyToken;
