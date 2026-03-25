507808a6b0e29f37a6ba04be65efbfac1adeff88
Singhnirmal11 <nirmal.nittinsaini@gmail.com>
2026-03-25 12:01:03 +0530

    Add faculty feedback form with API integration

--- client/src/components/FacultyList.js
+++ client/src/components/FacultyList.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 
 function FacultyList() {
+  const[selectedFaculty,setSelectedFaculty]= useState(null);
+  const[rating,setRating]=useState("");
+  const[comment,setComment]=useState("");
   const [faculties, setFaculties] = useState([]);
 
   useEffect(() => {
@@ -26,6 +29,36 @@ function FacultyList() {
     }
   };
 
+
+  const handleSubmit= async () =>{
+    try{
+      const token=localStorage.getItem("token");
+
+      const response=await fetch("http://localhost:5001/feedback",{
+        method: "POST",
+        headers:{
+          "Content-type": "application/json",
+          "Authorization": "Bearer "+ token
+        },
+        body: JSON.stringify({
+          faculty_id: selectedFaculty.id,
+          rating: rating,
+          comment: comment
+        })
+      });
+      
+      const data= await response.json();
+
+      if(response.ok){
+        alert("Feedback Submitted..");
+      }else{
+        alert(data.message);
+      }
+    } catch(error){
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h2>Faculty List</h2>
@@ -33,8 +66,37 @@ function FacultyList() {
       {faculties.map((faculty) => (
         <div key={faculty.id}>
           <p><b>{faculty.name}</b> - {faculty.department}</p>
+
+          <button onClick={() =>setSelectedFaculty(faculty)}>
+            Give Feedback
+          </button>
         </div>
       ))}
+
+      {selectedFaculty && (
+        <div>
+          <h3>Feedback for {selectedFaculty.name}</h3>
+
+          <input
+          type="number"
+          placeholder="rating (1-5)"
+          onChange={(e)=> setRating(e.target.value)}
+          />
+
+          <br /> <br />
+
+          <input
+          type="text"
+          placeholder="comment"
+          onChange={(e)=> setComment(e.target.value)}
+          />
+
+          <br /> <br />
+
+          <button onClick={handleSubmit}> Submit Feedback</button>
+        </div>
+      )}
+
     </div>
   );
 }
